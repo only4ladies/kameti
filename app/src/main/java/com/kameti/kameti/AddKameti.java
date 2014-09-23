8e91de6f88f5b33a33ac5e7f1ce8342254ca1304
Aayush Agrawal <aayush.nitb@gmail.com>
2014-09-23 10:37:20 +0530

    'Add Kameti' Layout

--- app/src/main/java/com/kameti/kameti/AddKameti.java
+++ app/src/main/java/com/kameti/kameti/AddKameti.java
@@ -6,37 +6,28 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class AddKameti extends Activity {
 
-    String kametiName = null;
-    TextView kametiNameView = null;
+    String kameti_name = null;
+    String kameti_start_date = null;
+    TextView value_kameti_name = null;
+    DatePicker value_kameti_start_date = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_kameti);
 
-        Button add = (Button) findViewById(R.id.add);
-        kametiNameView = (TextView) findViewById(R.id.kametiName);
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                kametiName = kametiNameView.getText().toString();
-                if(kametiName != null && !kametiName.isEmpty()) {
-                    KametiDbHelper kametiDbHelper = new KametiDbHelper(getBaseContext());
-                    SQLiteDatabase db = kametiDbHelper.getWritableDatabase();
-                    //ContentValues values = new ContentValues();
-                    //values.put("`name`", kametiName);
-                    //db.insert("`Kameti`", null, values);
-                    Intent intent = new Intent(getApplicationContext(), Kameties.class);
-                    startActivity(intent);
-                }
-            }
-        });
+        value_kameti_name = (TextView) findViewById(R.id.value_kamet_name);
+        value_kameti_start_date = (DatePicker) findViewById(R.id.value_kameti_start_date);
+
+        value_kameti_start_date.setCalendarViewShown(false);
     }
 
     @Override
@@ -45,4 +36,28 @@ public class AddKameti extends Activity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_save) {
+            kameti_name = value_kameti_name.getText().toString();
+            if(kameti_name == null || kameti_name.isEmpty()) {
+                return false;
+            }
+            kameti_start_date = "" + value_kameti_start_date.getYear()
+                                   + value_kameti_start_date.getMonth()
+                                   + value_kameti_start_date.getDayOfMonth();
+            /*
+            KametiDbHelper kametiDbHelper = new KametiDbHelper(getBaseContext());
+            SQLiteDatabase db = kametiDbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("`name`", kameti_name);
+            db.insert("`kameti`", null, values);
+            Intent intent = new Intent(getApplicationContext(), Kameties.class);
+            startActivity(intent);
+            finish();
+            */
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
